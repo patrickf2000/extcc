0082a331f27c8cdd576bbbf78d75488e0d829d39
patrickf2000 <dev@patrickflynn.co>
2020-04-06 12:09:22 -0400

    added float array test file (C)

---
+++ test/clang/array4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_array(float *numbers, int size) {
+	printf("[");
+	for (int i = 0; i<size; i++) {
+		float x = numbers[i];
+		printf("%.2f ", x);
+	}
+	puts("]");
+}
+
+int main() {
+	float *numbers = malloc(20);
+		
+	for (int i = 0; i<5; i++) {
+		numbers[i] = i * 10;
+	}
+	
+	print_array(numbers, 5);
+	
+	for (int i = 1; i<=5; i++) {
+		numbers[i] = i + 8;
+	}
+	
+	print_array(numbers, 5);
+	
+	free(numbers);
+	return 0;
+}

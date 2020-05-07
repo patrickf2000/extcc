27eb874375c0366029d19f4bdc02cf4fb038f6f9
patrickf2000 <dev@patrickflynn.co>
2020-05-07 09:48:30 -0400

    updated example file for floating-point instructions (LTAC-Asm)

--- examples/ltac-asm/float1.asm
+++ examples/ltac-asm/float1.asm
@@ -1,22 +1,26 @@
 section data
-	float no1 1.1
+	float no1 10.19
 	float no2 2.2
+	float answer 0.0
 	
 	string fmt "%f\n"
-	string fmt2 "%d\n"
 
 section code
 
 func main
 	var n1 float no1
-	var n2 int 10
-
-	pusharg string fmt
-	pusharg var n1
-	call printf
+	var n2 float no2
+	var result float answer
 	
-	pusharg string fmt2
-	pusharg var n2
+	fldr 1 n1
+	fldr 2 n2
+	
+	f32.add reg 1 reg 2
+	
+	fstr 1 result
+	
+	pusharg string fmt
+	pusharg var result
 	call printf
 
 	ret

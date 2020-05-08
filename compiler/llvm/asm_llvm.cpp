6a44b0fcf98cbaf1df533d77d960611c7191c859
patrickf2000 <dev@patrickflynn.co>
2020-05-08 08:44:37 -0400

    improved string handeling (LLVM)

--- compiler/llvm/asm_llvm.cpp
+++ compiler/llvm/asm_llvm.cpp
@@ -19,11 +19,35 @@ void Asm_LLVM::build_data(LtacDataSec *data) {
 			//Strings
 			case ltac::String: {
 				auto lstr = static_cast<LtacString *>(ln);
-				strings[lstr->name] = lstr->val.length();
+				std::string old_val = lstr->val;
+				std::string val = "";
+				int len = 0;
 				
+				//Check for special escape sequences
+				for (int i = 0; i<old_val.length(); i++) {
+					if (old_val[i] == '\\') {
+						if (old_val[i+1] == 'n') {
+							++i;
+							++len;
+							val += "\\0A";
+							continue;
+						}
+					}
+					
+					val += old_val[i];
+					++len;
+				}
+				
+				//Add the null characters
+				val += "\\00";
+				++len;
+				
+				//Write the code
 				writer << "@" << lstr->name << " = internal constant [";
-				writer << lstr->val.length() << " x i8] c\"" << lstr->val;
+				writer << len << " x i8] c\"" << val;
 				writer << "\"" << std::endl;
+				
+				strings[lstr->name] = len;
 			} break;
 		}
 	}

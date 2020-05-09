233e43467537750db40bfaa00467df5027357f67
patrickf2000 <dev@patrickflynn.co>
2020-05-09 08:38:51 -0400

    added partial support for jump instructions (LTAC-Asm)

--- lasm/lasm_flow.cpp
+++ lasm/lasm_flow.cpp
@@ -1,5 +1,7 @@
 #include "lasm_parser.hh"
 
+using namespace LtacAsm;
+
 //Builds a comparison statement
 void AsmParser::buildCmp() {
 	auto cmp = new LtacICmp;
@@ -11,5 +13,18 @@ void AsmParser::buildCmp() {
 
 //Builds a jump statement
 void AsmParser::buildJmp(int type) {
-
+	auto jmp = new LtacJmp;
+	file->code->children.push_back(jmp);
+	
+	Token dest = scan->getNext();
+	jmp->dest = dest.id;
+	
+	if (dest.type != AsmTokenType::Name)
+		syntax->fatalError("Expected label name.");
+		
+	switch (type) {
+		case AsmTokenType::Jl: jmp->op = Operator::Less; break;
+		
+		//TODO: Add rest
+	}
 }

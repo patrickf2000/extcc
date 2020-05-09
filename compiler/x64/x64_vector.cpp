e0a2481762b5a77fc6986d8bf86e0e98e332d343
patrickf2000 <dev@patrickflynn.co>
2020-05-09 09:23:10 -0400

    fixed segfault error when loading vector registers (x64)

--- compiler/x64/x64_vector.cpp
+++ compiler/x64/x64_vector.cpp
@@ -9,8 +9,8 @@ void Asm_x64::build_vload(LtacNode *node) {
 	auto vload = static_cast<LtacVLoad *>(node);
 	int reg = vload->reg - 1;
 	
-	writer << "\tvmovdqu " << vector_registers[reg];
-	writer << ", [rbp-" << vload->pos << "]" << std::endl;
+	writer << "\tmov rax, [rbp-" << vload->pos << "]" << std::endl;
+	writer << "\tvmovups " << vector_registers[reg] << ", [rax]" << std::endl;
 }
 
 //Build a vector store operation
@@ -18,7 +18,8 @@ void Asm_x64::build_vstore(LtacNode *node) {
 	auto vstore = static_cast<LtacVStore *>(node);
 	int reg = vstore->reg - 1;
 	
-	writer << "\tvmovdqu [rbp-" << vstore->pos << "], ";
+	writer << "\tmov rax, [rbp-" << vstore->pos << "]" << std::endl;
+	writer << "\tvmovups [rax], ";
 	writer << vector_registers[reg] << std::endl;
 }
 

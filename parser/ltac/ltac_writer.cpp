25decd10c0fd04e6961c53e52e54b9b473f95143
patrickf2000 <dev@patrickflynn.co>
2020-05-12 08:23:36 -0400

    added additional code for unwriter (ltac)

--- parser/ltac/ltac_writer.cpp
+++ parser/ltac/ltac_writer.cpp
@@ -28,15 +28,81 @@ void write_data(LtacNode *data) {
 	writer << std::endl;
 }
 
+//Translates a datatype to a string
+std::string type2str(DataType t) {
+	switch (t) {
+		case DataType::Int: return "int";
+		case DataType::Float: return "float";
+		case DataType::Double: return "double";
+		case DataType::Str: return "string";
+		case DataType::Void: return "void";
+		case DataType::Any: return "any";
+	}
+
+	return "";
+}
+
 //Writes the code section
 void write_code(LtacNode *code) {
 	writer << "section code" << std::endl;
 
 	for (auto ln : code->children) {
 		switch (ln->type) {
+			//Labels
+			case ltac::Label: {
+				auto lbl = static_cast<LtacLabel *>(ln);
+				writer << "lbl " << lbl->name << std::endl;
+			} break;
+
+			//Functions
 			case ltac::Func: {
 				auto func = static_cast<LtacFunc *>(ln);
 				writer << "func " << func->name << std::endl;
+			} break;
+
+			//External functions
+			case ltac::Extern: {
+				auto ext = static_cast<LtacExtern *>(ln);
+				writer << "extern " << type2str(ext->ret_type) << " ";
+				writer << ext->name;
+
+				for (auto p : ext->params) {
+					writer << " " << type2str(p);
+				}
+
+				writer << std::endl;
+			} break;
+
+			//Variables
+			case ltac::Var: {
+				auto var = static_cast<LtacVar *>(ln);
+				auto child = var->children[0];
+				std::string name = "var" + std::to_string(var->pos);
+
+				switch (child->type) {
+					//Store register operation
+					case ltac::Reg: {
+						auto reg = static_cast<LtacReg *>(child);
+
+						switch (var->d_type) {
+							case DataType::Int: writer << "\tstr "; break;
+							case DataType::Float: writer << "\tfstr "; break;
+							case DataType::Double: writer << "\tf64.str "; break;
+
+							//TODO: add the reset
+						}
+
+						writer << reg->pos << " " << name << std::endl;
+					} break;
+
+					//Assign one variable to another
+					case ltac::Var: {
+
+					} break;
+
+					//Other types
+					//TODO: Add
+				}
 			} break;
 		}
 	}

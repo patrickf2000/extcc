2180c5b2a57a1c59ceab38596bbd08ddc82c949d
patrickf2000 <dev@patrickflynn.co>
2020-05-25 14:50:40 -0400

    added string loading to constant pool (JVM)

--- compiler/jvm/jvm.cpp
+++ compiler/jvm/jvm.cpp
@@ -4,3 +4,29 @@ JVM::JVM(std::string name) {
 	builder = new JavaBuilder(name);
 	builder->useOutput();
 }
+
+//Build the data section
+void JVM::build_data(PasmFile *file) {
+	for (auto item : file->data) {
+		auto str = static_cast<PasmString *>(item);
+	
+		switch (item->type) {
+			case pasm::String: builder->addString(str->val); break;
+		}
+	}
+}
+
+//Build function declaration
+void JVM::build_func(PasmNode *ln) {
+
+}
+
+//Build function return
+void JVM::build_ret() {
+
+}
+
+//Build function call
+void JVM::build_call(PasmNode *ln) {
+
+}
--- compiler/jvm/jvm.hh
+++ compiler/jvm/jvm.hh
@@ -3,14 +3,22 @@
 #include <string>
 
 #include <base/cbase.hh>
+#include <pasm/pasm.hh>
 
 #include <javabuilder.hh>
 #include <javafile.hh>
 #include <javapool.hh>
 
+using namespace PASM;
+
 class JVM : public CompilerBase {
 public:
 	explicit JVM(std::string name);
+	void build_data(PasmFile *file);
+	
+	void build_func(PasmNode *ln);
+	void build_ret();
+	void build_call(PasmNode *ln);
 	
 	void code_init() {}
 	void write() {
